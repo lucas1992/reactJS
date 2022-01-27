c15efb2e743e6cae3bb0ff196f45288935b80d52
lucas1992 <lucas.etcheverry28@gmail.com>
2022-01-27 01:04:27 -0300

    Se agrega código y validaciones.

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -2,22 +2,56 @@ import { Button } from 'react-bootstrap';
 import AddIcon from "@material-ui/icons/Add";
 import RemoveIcon from "@material-ui/icons/Remove";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { useState, useEffect } from "react";
+import './ItemCount.css';
 
-function ItemCount() {
+const ItemCount = ({ stock, initial = 0,  onAdd }) => {
+  const [cartCount, setCartCount] = useState(parseInt(initial));
+
+
+  // Igualmente con el valor por default ya quedaba cubierta la funcionalidad
+  useEffect(() => {
+        // prevent undefined and stock=0. En la clase se omitio esto.
+        if(initial && parseInt(stock) !== 0){
+             setCartCount(parseInt(initial));
+        }else{
+            setCartCount(0);
+        }
+  }, []);
+
+
+  const decrement = () => {
+    if(cartCount === 0){
+        return;
+    }
+    setCartCount(cartCount - 1);
+  }
+
+  const increment = () => {
+    if(cartCount === parseInt(stock)){
+        return;
+    }
+    setCartCount(cartCount + 1);
+  }
+
   return (
     <>
         <ButtonGroup className="ml-1">
-          <Button variant="dark">
+          <Button variant="dark" onClick={decrement}>
             {" "}
             <RemoveIcon fontSize="small" />
           </Button>
-          <label>1</label>
-          <Button variant="dark">
+          <label className="centerLabel">{ cartCount }</label>
+          <Button variant="dark" onClick={increment}>
             {" "}
             <AddIcon fontSize="small" />
           </Button>
         </ButtonGroup>
-        <Button className="ml--2" variant="primary">Agregar al carrito</Button>
+        {
+            parseInt(stock)
+            ? <Button className="ml--2" variant="primary" onClick={() => onAdd(cartCount)}>Agregar al carrito</Button>
+            : <Button className="ml--2" variant="primary" disabled>Agregar al carrito</Button>
+        }
     </>
   );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,12 +1,17 @@
 import ItemCount from './ItemCount';
 
+function showCartCount(varCount){
+    alert(varCount);
+}
+
 function ItemListContainer(props) {
   return (
     <>
         <h3 className="ml-1 mt-2"> {props.greeting} </h3>
-        <ItemCount />
+        <ItemCount stock="5" initial="1" onAdd={showCartCount}/>
     </>
   );
 }
+
 
 export default ItemListContainer;
